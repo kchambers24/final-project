4341221d6ee9d3221d3ddf20abc090214cc61ab2
Kevin Chambers <kbchambers2@gmail.com>
2016-10-07 12:41:53 -0400

    Updated InviteLogin file

--- src/InviteLogin.js
+++ src/InviteLogin.js
@@ -8,46 +8,52 @@ class InviteLogin extends Component {
       <div className="Login">
         <div className="loginNav">
           <div className="teamContact">
-            <div className="logoNav"><a className="logoNavLink" href="#Home">
-              <img src="http://i64.tinypic.com/2rpfv6g.png" alt="Logo"/></a>
+            <div className="logoNav">
+              <a className="logoNavLink" href="#Home">
+                <img src="http://i64.tinypic.com/2rpfv6g.png" alt="Logo"/></a>
             </div>
             <div className="teamContactBox">
-            <div className="team"><a className="aTeam" href="#team">Team</a></div>
-            <div className="contact"><a className="aTeam" href="#Contact">Contact</a></div>
+              <div className="team">
+                <a className="aTeam" href="#team">Team</a>
+              </div>
+              <div className="contact">
+                <a className="aTeam" href="#Contact">Contact</a>
+              </div>
             </div>
           </div>
         </div>
         <div className="loginHouseName" id="Home">
           <div className="logoSignBox">
-          <div className="logoBox">
-          </div>
-          <div className="householdSignInBox">
-            <div className="opacityBox">
-              <div className="title">
-                <img className="roomieLogo" src="http://i67.tinypic.com/2cmlxfb.png" alt="ROOMIE"/>
+            <div className="logoBox"></div>
+            <div className="householdSignInBox">
+              <div className="opacityBox">
+                <div className="title">
+                  <img className="roomieLogo" src="http://i67.tinypic.com/2cmlxfb.png" alt="ROOMIE"/>
+                </div>
+                <div className="chooseName">
+                  <div className="chooseText">
+                    <p className="loginTextHeader">
+                      <strong>Welcome to ROOMIE</strong>
+                    </p>
+                    <p className="loginTextBody">
+                      <small>Continue with Facebook</small>
+                    </p>
+                    <br/>
+                  </div>
+                </div>
+                <div>
+                  <button className="loginBtn" onClick={this.props.onLogin}>
+                    <i className="fa fa-facebook-square fa" aria-hidden="true"></i>&nbsp;Sign in with Facebook</button>
+                </div>
               </div>
-            <div className="chooseName">
-              <div className="chooseText">
-                <p className="loginTextHeader"><strong>Welcome to ROOMIE</strong></p>
-                <p className="loginTextBody"><small>Continue with Facebook</small></p>
-                <br/>
-              </div>
             </div>
-
-            <div>
-              <button className="loginBtn" onClick={this.props.onLogin}>
-              <i className="fa fa-facebook-square fa-2x" aria-hidden="true">  Sign in with Facebook</i>
-              </button>
-              <br/>
-            </div>
-            </div>
-          </div>
           </div>
         </div>
         <div className="bodyBox">
           <div className="bodyTitle">
             <p className="solutionText">A Simple Solution for All Your Roommate Needs</p>
-            <p className="bodySecondText">Co-Habitation. Simplified.</p></div>
+            <p className="bodySecondText">Co-Habitation. Simplified.</p>
+          </div>
           <div className="loginBody">
             <ul className="cleaningImages">
               <li className="smallerCleaningImages">
@@ -55,13 +61,13 @@ class InviteLogin extends Component {
                 <p className="choreText">Dust The House</p>
               </li>
               <li className="smallerCleaningImages"><img src="http://i68.tinypic.com/2v1m9h2.png" alt="Dont be lazy" height="199" width="200"/>
-              <p className="choreText">Take Out The Trash</p>
+                <p className="choreText">Take Out The Trash</p>
               </li>
               <li className="smallerCleaningImages"><img src="http://i63.tinypic.com/33o3yhw.png" alt="Dont be lazy" height="199" width="200"/>
-              <p className="choreText">Clean The Kitchen</p>
+                <p className="choreText">Clean The Kitchen</p>
               </li>
               <li className="smallerCleaningImages"><img src="http://i67.tinypic.com/1zo86f5.png" alt="Dont be lazy" height="199" width="200"/>
-              <p className="choreText">Water The Plants</p>
+                <p className="choreText">Water The Plants</p>
               </li>
             </ul>
           </div>
@@ -95,47 +101,47 @@ class InviteLogin extends Component {
         </div>
         <div className="contactFooter" id="Contact">
           <div className="contactBox">
-          <div className="teamSocial">
-          <h3>Contact Linsy</h3>
-          <ul className="socialIcons">
-            <li className="listSocial">
-              <a href="https://www.facebook.com/profile.php?id=100013525780488&fref=ts" className="btn-social btn-outline">
-                <i className="fa fa-facebook-square" aria-hidden="true"></i>
-              </a>
-            </li>
-            <li className="listSocial">
-              <a href="https://github.com/linsydeanna" className="btn-social btn-outline">
-                <i className="fa fa-github-square" aria-hidden="true"></i>
-              </a>
-            </li>
-            <li className="listSocial">
-              <a href="https://www.linkedin.com/in/linsydeanna?authType=NAME_SEARCH&authToken=2GqI&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A435101060%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1475602726237%2Ctas%3Alin" className="btn-social btn-outline">
-                <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="teamSocial">
-        <h3>Contact Kevin</h3>
-        <ul className="socialIcons">
-          <li className="listSocial">
-            <a href="https://www.facebook.com/kevin.chambers.507" className="btn-social btn-outline">
-              <i className="fa fa-facebook-square" aria-hidden="true"></i>
-            </a>
-          </li>
-          <li className="listSocial">
-            <a href="https://github.com/kbchambers2" className="btn-social btn-outline">
-              <i className="fa fa-github-square" aria-hidden="true"></i>
-            </a>
-          </li>
-          <li className="listSocial">
-            <a href="https://linkedin.com/in/kbchambers" className="btn-social btn-outline">
-              <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-        </div>
+            <div className="teamSocial">
+              <h3>Contact Linsy</h3>
+              <ul className="socialIcons">
+                <li className="listSocial">
+                  <a href="https://www.facebook.com/profile.php?id=100013525780488&fref=ts" className="btn-social btn-outline">
+                    <i className="fa fa-facebook-square" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li className="listSocial">
+                  <a href="https://github.com/linsydeanna" className="btn-social btn-outline">
+                    <i className="fa fa-github-square" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li className="listSocial">
+                  <a href="https://www.linkedin.com/in/linsydeanna?authType=NAME_SEARCH&authToken=2GqI&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A435101060%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1475602726237%2Ctas%3Alin" className="btn-social btn-outline">
+                    <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="teamSocial">
+              <h3>Contact Kevin</h3>
+              <ul className="socialIcons">
+                <li className="listSocial">
+                  <a href="https://www.facebook.com/kevin.chambers.507" className="btn-social btn-outline">
+                    <i className="fa fa-facebook-square" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li className="listSocial">
+                  <a href="https://github.com/kbchambers2" className="btn-social btn-outline">
+                    <i className="fa fa-github-square" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li className="listSocial">
+                  <a href="https://linkedin.com/in/kbchambers" className="btn-social btn-outline">
+                    <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
 
         </div>
         <div className="loginFooter">
@@ -149,7 +155,7 @@ class InviteLogin extends Component {
 }
 
 InviteLogin.contextTypes = {
- household: React.PropTypes.string
+  household: React.PropTypes.string
 }
 
 export default InviteLogin;
